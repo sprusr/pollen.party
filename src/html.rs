1fed232f662e3bd364a1b5d5d05ae38ce2cf9468
Scott Street <scott@spru.sr>
2024-04-11 19:12:29 +0300

    Refactor markup rendering to html.rs

---
+++ src/html.rs
@@ -0,0 +1,87 @@
+use chrono::{DateTime, Locale, Utc};
+use chrono_tz::Tz;
+use maud::{html, Markup, DOCTYPE};
+
+use crate::silam::Pollen;
+
+pub fn page(back_enabled: bool, fetched_at: DateTime<Utc>, content: Markup) -> Markup {
+    html! {
+        (DOCTYPE)
+        html lang="en" {
+            head {
+                meta charset="utf-8";
+                title { "pollen.party" }
+                meta name="viewport" content="width=device-width, initial-scale=1" {}
+                link  rel="stylesheet" href="style.css" {}
+                script src="script.js" defer {}
+            }
+            body {
+                header {
+                    h1 { "⚘ " a href="/" { "pollen.party" } " ⚘" }
+                    @if back_enabled {
+                        p { small { a href="/" { "‹ Back" } } }
+                    }
+                }
+                main {
+                    (content)
+                }
+                footer {
+                    p {
+                        small {
+                            "Data was fetched at: "
+                            (fetched_at)
+                            ". For enquiries contact webmaster at this domain."
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub fn home() -> Markup {
+    html! {
+        p {
+            "This website provides pollen forecasts for Europe. "
+            "Times displayed in location's local timezone. Data from "
+            a href="https://silam.fmi.fi/" { "FMI SILAM" }
+            " and "
+            a href="https://www.polleninfo.org/" { "EAN" }
+            ". Search uses "
+            a href="https://www.openstreetmap.org/copyright" { "OpenStreetMap" }
+            "."
+        }
+        form action="" method="GET" id="geo-form" {
+            input type="button" value="Use my location" id="geo" class="big";
+            input type="hidden" name="lat" id="lat";
+            input type="hidden" name="lon" id="lon";
+        }
+        p class="center" { "or" }
+        form action="" method="GET" {
+            label for="loc" { "Location" }
+            input type="text" name="loc" id="loc" placeholder="E.g. Helsinki, Finland" required;
+            input type="submit" value="Search";
+        }
+    }
+}
+
+pub fn forecast(pollen: &Vec<Pollen>, location: &String, timezone: &Tz, locale: &Locale) -> Markup {
+    html! {
+        h2 { (location) }
+        p {
+            "Data from "
+            a href="https://silam.fmi.fi/" { "FMI SILAM" }
+            " and "
+            a href="https://www.polleninfo.org/" { "EAN" }
+            "."
+        }
+        table {
+            @for p in pollen {
+                tr {
+                    td { (p.time.with_timezone(timezone).format_localized("%a %R", *locale)) }
+                    td { (p.pollen_index) " (" (p.pollen_index_source) ")" }
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use axum::{
 };
 use chrono::{Local, Locale, NaiveTime};
 use chrono_tz::Tz;
-use maud::{html, DOCTYPE};
+use html::page;
 use nominatim::{Client, IdentificationMethod};
 use reqwest::header;
 use reverse_geocoder::ReverseGeocoder;
@@ -22,9 +22,13 @@ use tokio::time;
 use tower_http::services::ServeDir;
 use tzf_rs::DefaultFinder;
 
+mod html;
 mod silam;
 
-use crate::silam::{Pollen, Silam};
+use crate::{
+    html::{forecast, home},
+    silam::{Pollen, Silam},
+};
 
 struct AppState {
     finder: DefaultFinder,
@@ -175,77 +179,15 @@ async fn index(Query(params): Query<Params>, State(state): State<Arc<AppState>>)
     let mut headers = HeaderMap::new();
     headers.insert(header::CACHE_CONTROL, cache_control.parse().unwrap());
 
-    let body = html! {
-        (DOCTYPE)
-        html lang="en" {
-            head {
-                meta charset="utf-8";
-                title { "pollen.party" }
-                meta name="viewport" content="width=device-width, initial-scale=1" {}
-                link  rel="stylesheet" href="style.css" {}
-                script src="script.js" defer {}
-            }
-            body {
-                header {
-                    h1 { "⚘ " a href="/" { "pollen.party" } " ⚘" }
-                    @if result.is_some() {
-                        p { small { a href="/" { "‹ Back" } } }
-                    }
-                }
-                main {
-                    @if let Some((pollen, location, timezone)) = result {
-                        h2 { (location) }
-                        p {
-                            "Data from "
-                            a href="https://silam.fmi.fi/" { "FMI SILAM" }
-                            " and "
-                            a href="https://www.polleninfo.org/" { "EAN" }
-                            "."
-                        }
-                        table {
-                            @for p in &pollen {
-                                tr {
-                                    td { (p.time.with_timezone(&timezone).format_localized("%a %R", locale)) }
-                                    td { (p.pollen_index) " (" (p.pollen_index_source) ")" }
-                                }
-                            }
-                        }
-                    } @else {
-                        p {
-                            "This website provides pollen forecasts for Europe. "
-                            "Times displayed in location's local timezone. Data from "
-                            a href="https://silam.fmi.fi/" { "FMI SILAM" }
-                            " and "
-                            a href="https://www.polleninfo.org/" { "EAN" }
-                            ". Search uses "
-                            a href="https://www.openstreetmap.org/copyright" { "OpenStreetMap" }
-                            "."
-                        }
-                        form action="" method="GET" id="geo-form" {
-                            input type="button" value="Use my location" id="geo" class="big";
-                            input type="hidden" name="lat" id="lat";
-                            input type="hidden" name="lon" id="lon";
-                        }
-                        p class="center" { "or" }
-                        form action="" method="GET" {
-                            label for="loc" { "Location" }
-                            input type="text" name="loc" id="loc" placeholder="E.g. Helsinki, Finland" required;
-                            input type="submit" value="Search";
-                        }
-                    }
-                }
-                footer {
-                    p {
-                        small {
-                            "Data was fetched at: "
-                            (state.silam.read().unwrap().fetch_time)
-                            ". For enquiries contact webmaster at this domain."
-                        }
-                    }
-                }
-            }
-        }
-    };
+    let body = page(
+        result.is_some(),
+        state.silam.read().unwrap().fetch_time,
+        if let Some((pollen, location, timezone)) = result {
+            forecast(&pollen, &location, &timezone, &locale)
+        } else {
+            home()
+        },
+    );
 
     (headers, body).into_response()
 }
